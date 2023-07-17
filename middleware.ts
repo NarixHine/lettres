cb1c96cb537e0664566ac3b80d05628960bdef32
Narix Hine <127665924+NarixHine@users.noreply.github.com>
2023-07-18 01:19:23 +0800

    Debugging

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,9 @@ import { authMiddleware, redirectToSignIn } from '@clerk/nextjs'
 
 export default authMiddleware({
     afterAuth(auth, req) {
+        console.log(JSON.stringify(auth))
         if (!auth.isPublicRoute && !auth.userId) {
+            console.log('not logged in')
             return redirectToSignIn({ returnBackUrl: req.url })
         }
     },
@@ -10,5 +12,5 @@ export default authMiddleware({
 })
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 }
